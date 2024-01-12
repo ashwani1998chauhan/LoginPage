ee111919d1a7c47a36943eca74385c259a4954e5
41019802 <41019802@ASHWANICHAUHAN>
2024-01-12 17:03:56 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginPage.Models;
+using System.Collections.Generic;
+
+namespace LoginPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<UserModel> _users = new List<UserModel>
+        {
+            new UserModel { Username = "123", Password = "Password123" },
+          
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                var user = _users.Find(u => u.Username == model.Username && u.Password == model.Password);
+
+                if (user != null)
+                {
+                   
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid username or password");
+                }
+
+
+            }
+
+        
+            return View("Index", model);
+        }
+
+        public IActionResult DownloadUserManual()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files/UserManual.pdf");
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var fileContent = System.IO.File.ReadAllBytes(filePath);
+                var contentType = "application/pdf"; // Set the content type for PDF files
+                var fileName = "UserManual.pdf";
+
+                return File(fileContent, contentType, fileName);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace LoginPage.Models
+{
+
+    public class UserModel
+    {
+       
+        [Required(ErrorMessage = "Username is required.")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "Username must contain only numbers.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$",
+            ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+
+}
